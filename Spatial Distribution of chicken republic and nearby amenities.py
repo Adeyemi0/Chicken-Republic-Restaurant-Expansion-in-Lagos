717e9679c1d1c9c4de072e4932e6b89213dae1d0
Adediran Adeyemi <65359919+Adeyemi0@users.noreply.github.com>
2025-03-29 09:42:11 +0100

    Create Spatial Distribution of chicken republic and nearby amenities.py

---
+++ Spatial Distribution of chicken republic and nearby amenities.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+def main():
+    st.title("Chicken Republic Map in Streamlit")
+
+    # Read the HTML file
+    with open("Spatial Distribution of Chicken Republic Branches and Nearby Amenities in Lagos.html", "r", encoding="utf-8") as f:
+        html_string = f.read()
+
+    # Embed the HTML in Streamlit
+    components.html(html_string, width=800, height=600, scrolling=True) #adjust width and height as needed.
+
+if __name__ == "__main__":
+    main()￼Enter
